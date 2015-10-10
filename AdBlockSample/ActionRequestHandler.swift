6cafc17d5510dba7902c02a68e4facb3274147a5
nakazy <s.nakajima0523@gmail.com>
2015-10-10 23:36:52 +0900

    アドブロックのサンプルを追加

---
+++ AdBlockSample/ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  AdBlockSample
+//
+//  Created by nakazy on 2015/10/10.
+//  Copyright © 2015年 nakazy. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}
