ba74afddf7564e94a0b721910784fe45bd53db9b
nakazy <s.nakajima0523@gmail.com>
2015-10-11 01:41:49 +0900

    大体動作確認できた

--- AdBlcok/ViewController.swift
+++ AdBlcok/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        SFContentBlockerManager.reloadContentBlockerWithIdentifier("com.nakailand.AdBlcok") {
+            error in
+            print(error)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
